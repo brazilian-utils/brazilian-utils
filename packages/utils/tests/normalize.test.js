146c4230141f332c4a58b0c2a617a3ec00d852eb
Lucas Veloso <lucas.s.veloso@hotmail.com>
2018-06-12 22:45:20 -0300

    Improve tests of isValidCpf, isValidCNPJ and normalize util

---
+++ packages/utils/tests/normalize.test.js
@@ -0,0 +1,7 @@
+import normalize from '../normalize';
+
+describe('normalize', () => {
+  test('should remove all characters that are not numbers', () => {
+    expect(normalize('123abc456?.#789xyz 0')).toBe('1234567890');
+  })
+});

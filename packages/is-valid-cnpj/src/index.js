a54e687ed4928d7b1d769c4366e34934dea4ce58
Andreo Vieira <contato@andreovieira.com.br>
2018-06-14 23:24:46 -0300

    chore(tooling): Add prettier and adjust npm scripts.

--- packages/is-valid-cnpj/src/index.js
+++ packages/is-valid-cnpj/src/index.js
@@ -7,17 +7,18 @@ const isValidLength = cnpj => cnpj.length === CNPJ_LENGTH;
 const belongsToBlacklist = cnpj => BLACKLIST.includes(cnpj);
 
 // inspired by https://github.com/cyberglot/bradoc/blob/master/lib/cnpj.js#L6
-const isValidChecksum = cnpj => CHECK_DIGITS.every((index) => {
-  const digits = cnpj.slice(0, index).split('');
+const isValidChecksum = cnpj =>
+  CHECK_DIGITS.every(index => {
+    const digits = cnpj.slice(0, index).split('');
 
-  const weights = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+    const weights = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
 
-  if (index === CHECK_DIGITS[CHECK_DIGITS.length - 1]) weights.unshift(6);
+    if (index === CHECK_DIGITS[CHECK_DIGITS.length - 1]) weights.unshift(6);
 
-  const mod = digits.reduce((acc, digit, i) => acc + (digit * weights[i]), 0) % 11;
+    const mod = digits.reduce((acc, digit, i) => acc + digit * weights[i], 0) % 11;
 
-  return cnpj[index] === String((mod < 2 ? 0 : 11 - mod));
-});
+    return cnpj[index] === String(mod < 2 ? 0 : 11 - mod);
+  });
 
 export default function isValidCnpj(cnpj) {
   if (!cnpj) return false;

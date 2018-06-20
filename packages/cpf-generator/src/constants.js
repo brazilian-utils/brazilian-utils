c4d06a783b69a114d6410632540acd17f0699b40
Matheus Almeida <mat.almeida@live.com>
2018-06-19 23:26:58 -0300

    created test suite to CPF Generator

---
+++ packages/cpf-generator/src/constants.js
@@ -0,0 +1,33 @@
+export const CPF_LENGTH_WITHOUT_MASK = 11;
+
+export const CPF_LENGTH_WITH_MASK = 14;
+
+export const STATE_CODE = {
+  'RS': '0',
+  'DF': '1',
+  'GO': '1',
+  'MT': '1',
+  'MS': '1',
+  'TO': '1',
+  'AM': '2',
+  'PA': '2',
+  'RR': '2',
+  'AP': '2',
+  'AC': '2',
+  'RO': '2',
+  'CE': '3',
+  'MA': '3',
+  'PI': '3',
+  'PB': '4',
+  'PE': '4',
+  'AL': '4',
+  'RN': '4',
+  'BA': '5',
+  'SE': '5',
+  'MG': '6',
+  'RJ': '7',
+  'ES': '7',
+  'SP': '8',
+  'PR': '9',
+  'SC': '9',
+}

6b5ac42eb1d3e500806ba0a57b254796ce5baa86
Lucas Veloso <lucas.s.veloso@hotmail.com>
2018-06-22 22:00:42 -0300

    create package helper-is-last-char

--- packages/format-cnpj/src/index.js
+++ packages/format-cnpj/src/index.js
@@ -1,8 +1,7 @@
 import onlyNumbers from '@brazilian-utils/helper-only-numbers';
+import isLastChar from '@brazilian-utils/helper-is-last-char';
 
 import { CNPJ_LENGTH, DOT_INDEXES, SLASH_INDEXES, HYPHEN_INDEXES } from './constants';
-
-const isLastDigit = (cnpj, index) => index === cnpj.length - 1;
 
 export default function formatCnpj(cnpj) {
   if (!cnpj) return '';
@@ -14,7 +13,7 @@ export default function formatCnpj(cnpj) {
     .split('')
     .reduce((acc, digit, index) => {
       const result = `${acc}${digit}`;
-      if (!isLastDigit(numericCNPJ, index)) {
+      if (!isLastChar(index, numericCNPJ)) {
         if (DOT_INDEXES.includes(index)) return `${result}.`;
         if (SLASH_INDEXES.includes(index)) return `${result}/`;
         if (HYPHEN_INDEXES.includes(index)) return `${result}-`;

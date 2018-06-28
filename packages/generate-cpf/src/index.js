7ede03a506b8c74739476d77e04c80dd31404451
Matheus Almeida <mat.almeida@live.com>
2018-06-28 20:16:12 -0300

    update package to use the helper package

--- packages/generate-cpf/src/index.js
+++ packages/generate-cpf/src/index.js
@@ -1,7 +1,5 @@
+import generateChecksum from '@brazilian-utils/helper-generate-checksum';
 import { CPF_LENGTH, STATES_CODE } from './constants';
-
-const createCheckSum = (cpfStart, base) =>
-  cpfStart.split('').reduce((agg, value, index) => agg + parseInt(value, 10) * (base - index), 0);
 
 const randomNumber = length =>
   Math.random()
@@ -12,10 +10,10 @@ export default function generateCpf({ state } = {}) {
   const stateCode = Object.keys(STATES_CODE).includes(state) ? STATES_CODE[state] : randomNumber(1);
   const baseCpf = randomNumber(CPF_LENGTH - 3) + stateCode;
 
-  const mod1 = createCheckSum(baseCpf, 10) % 11;
+  const mod1 = generateChecksum(baseCpf, 10) % 11;
   const check1 = (mod1 < 2 ? 0 : 11 - mod1).toString();
 
-  const mod2 = createCheckSum(baseCpf + check1, 11) % 11;
+  const mod2 = generateChecksum(baseCpf + check1, 11) % 11;
   const check2 = (mod2 < 2 ? 0 : 11 - mod2).toString();
 
   return `${baseCpf}${check1.toString()}${check2.toString()}`;

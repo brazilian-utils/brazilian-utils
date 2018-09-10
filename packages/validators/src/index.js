a2055a1cbcb3a27220100021bbc6f8c459e23f2b
Hyan Mandian <hyanmandian@hotmail.com>
2018-09-10 13:35:03 -0300

    Add CEP and Boleto validators to validators package

--- packages/validators/src/index.js
+++ packages/validators/src/index.js
@@ -1,2 +1,4 @@
 export { default as isValidCpf } from '@brazilian-utils/is-valid-cpf';
 export { default as isValidCnpj } from '@brazilian-utils/is-valid-cnpj';
+export { default as isValidBoleto } from '@brazilian-utils/is-valid-boleto';
+export { default as isValidCep } from '@brazilian-utils/is-valid-cep';

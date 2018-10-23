f75b51d75186874d0e03f88a3c803ba7c2c81e67
Lucas <lucasvalenteds@gmail.com>
2018-10-23 14:21:12 -0300

    Replace duplicated code with call to helper module

--- packages/generate-cnpj/src/index.js
+++ packages/generate-cnpj/src/index.js
@@ -1,11 +1,11 @@
-import { generateRandomNumbers, multiplyWeights, sumNumbers, calculateNthDigit, concatDigits } from './cnpj';
+import { generateRandomNumbers, sumOfDigitsMultipliedByWeights, calculateNthDigit, concatDigits } from './cnpj';
 import { CNPJ_LENGTH, FIRST_DIGIT_WEIGHTS, SECOND_DIGIT_WEIGHTS } from './constants';
 
 export default function generateCnpj() {
   const twelveNumbers = generateRandomNumbers(CNPJ_LENGTH - 2);
-  const thirteenth = calculateNthDigit(sumNumbers(multiplyWeights(twelveNumbers, FIRST_DIGIT_WEIGHTS)));
+  const thirteenth = calculateNthDigit(sumOfDigitsMultipliedByWeights(twelveNumbers, FIRST_DIGIT_WEIGHTS));
   const fourteenth = calculateNthDigit(
-    sumNumbers(multiplyWeights([...twelveNumbers, thirteenth], SECOND_DIGIT_WEIGHTS))
+    sumOfDigitsMultipliedByWeights([...twelveNumbers, thirteenth].join(''), SECOND_DIGIT_WEIGHTS)
   );
 
   return concatDigits([...twelveNumbers, thirteenth, fourteenth]);

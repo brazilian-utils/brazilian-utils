5bed4ebfbd903b5ff57bfca36b0370679a0b5f42
Lucas <lucasvalenteds@gmail.com>
2018-10-23 14:47:10 -0300

    Merge `cnpj.js` and `index.js` files

--- packages/generate-cnpj/src/index.js
+++ packages/generate-cnpj/src/index.js
@@ -1,5 +1,26 @@
-import { generateRandomNumbers, sumOfDigitsMultipliedByWeights, calculateNthDigit, concatDigits } from './cnpj';
+import generateChecksum from '@brazilian-utils/helper-generate-checksum';
+
 import { CNPJ_LENGTH, FIRST_DIGIT_WEIGHTS, SECOND_DIGIT_WEIGHTS } from './constants';
+
+export const generateRandomNumbers = n =>
+  Array(n)
+    .fill(0)
+    .map(() =>
+      Math.random()
+        .toString()
+        .substr(2, 1)
+    )
+    .map(number => parseInt(number, 10));
+
+export const sumOfDigitsMultipliedByWeights = (numbers, weights) => generateChecksum(numbers, weights);
+
+export const calculateNthDigit = sum => {
+  const mod = sum % 11;
+
+  return Math.abs(mod < 2 ? 0 : mod - 11);
+};
+
+export const concatDigits = digits => digits.reduce((string, digit) => string + digit, '');
 
 export default function generateCnpj() {
   const twelveNumbers = generateRandomNumbers(CNPJ_LENGTH - 2);

39d9ec216815f4c65628bb1d1195c4ad372f178b
Victor Magalhães <victorhfmag@gmail.com>
2018-10-31 08:21:24 -0300

    ♻️ Setup typescript and tslint tools, remove eslint and setup new build

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   collectCoverage: true,
   coverageDirectory: './coverage/',
-  collectCoverageFrom: ['**/src/*.js', '!**/node_modules/**', '!**/dist/**'],
+  collectCoverageFrom: ['**/src/*.(js|ts)', '!**/node_modules/**', '!**/dist/**'],
+  preset: 'ts-jest/presets/js-with-babel',
+  coveragePathIgnorePatterns: ['.*.d.ts'],
+  moduleNameMapper: {
+    '@taxi/(.+)$': '<rootDir>packages/$1/src',
+  },
 };

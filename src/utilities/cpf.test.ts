620f7989b11bbeb3bf78f8c409250a3add3b0a8c
Andreo Vieira <contato@andreovieira.com.br>
2019-09-19 22:29:38 -0300

    feat(CPF): migrate isValidCPF

--- src/utilities/cpf.test.ts
+++ src/utilities/cpf.test.ts
@@ -1,5 +1,5 @@
 import { STATES, STATES_CODE } from '../common/states';
-import { formatCPF, generateCPF, CPF_LENGTH } from './cpf';
+import { formatCPF, generateCPF, isValidCPF, CPF_LENGTH, RESERVED_NUMBERS } from './cpf';
 
 describe('formatCPF', () => {
   test('should format CPF with mask', () => {
@@ -41,5 +41,64 @@ describe('generateCPF', () => {
         expect(generateCPF(state).substr(8, 1) === STATES_CODE[state]).toBe(true);
       })
     );
+  });
+});
+
+describe('isValidCPF', () => {
+  describe('should return false', () => {
+    test('when it is on the BLACKLIST', () => {
+      RESERVED_NUMBERS.forEach(cpf => expect(isValidCPF(cpf)).toBe(false));
+    });
+
+    test('when is a empty string', () => {
+      expect(isValidCPF('')).toBe(false);
+    });
+
+    test('when is null', () => {
+      expect(isValidCPF(null as any)).toBe(false);
+    });
+
+    test('when is undefined', () => {
+      expect(isValidCPF(undefined as any)).toBe(false);
+    });
+
+    test('when is a boolean', () => {
+      expect(isValidCPF(true as any)).toBe(false);
+      expect(isValidCPF(false as any)).toBe(false);
+    });
+
+    test('when is a object', () => {
+      expect(isValidCPF({} as any)).toBe(false);
+    });
+
+    test('when is a array', () => {
+      expect(isValidCPF([] as any)).toBe(false);
+    });
+
+    test(`when dont match with CPF length (${CPF_LENGTH})`, () => {
+      expect(isValidCPF('123456')).toBe(false);
+    });
+
+    test('when contains only letters or special characters', () => {
+      expect(isValidCPF('abcabcabcde')).toBe(false);
+    });
+
+    test('when is a CPF invalid', () => {
+      expect(isValidCPF('11257245286')).toBe(false);
+    });
+
+    test('when is a CPF invalid test numbers with letters', () => {
+      expect(isValidCPF('foo391.838.38test0-66')).toBe(false);
+    });
+  });
+
+  describe('should return true', () => {
+    test('when is a CPF valid without mask', () => {
+      expect(isValidCPF('40364478829')).toBe(true);
+    });
+
+    test('when is a CPF valid with mask', () => {
+      expect(isValidCPF('962.718.458-60')).toBe(true);
+    });
   });
 });

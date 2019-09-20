43f3e93ee6c81dd495a75c170aec2564923efa9d
Andreo Vieira <contato@andreovieira.com.br>
2019-09-19 22:29:38 -0300

    feat(CPF): migrate isValidCPF

--- src/utilities/cpf.ts
+++ src/utilities/cpf.ts
@@ -2,8 +2,25 @@ import { State, STATES, STATES_CODE } from '../common/states';
 import { onlyNumbers, isLastChar, randomNumber, generateChecksum } from '../helpers';
 
 export const CPF_LENGTH = 11;
+
 export const DOT_INDEXES = [2, 5];
+
 export const HYPHEN_INDEXES = [8];
+
+export const CHECK_DIGITS = [9, 10];
+
+export const RESERVED_NUMBERS = [
+  '00000000000',
+  '11111111111',
+  '22222222222',
+  '33333333333',
+  '44444444444',
+  '55555555555',
+  '66666666666',
+  '77777777777',
+  '88888888888',
+  '99999999999',
+];
 
 export function formatCPF(cpf: string) {
   const digits = onlyNumbers(cpf);
@@ -34,4 +51,28 @@ export function generateCPF(state?: State) {
   const check2 = (mod2 < 2 ? 0 : 11 - mod2).toString();
 
   return `${baseCPF}${check1.toString()}${check2.toString()}`;
+}
+
+const isValidCpfFormat = (cpf: string) => /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/.test(cpf);
+const isReservedNumber = (cpf: string) => RESERVED_NUMBERS.indexOf(cpf) >= 0;
+
+// TODO: move to checksum helper
+export function isValidChecksum(cpf: string) {
+  return CHECK_DIGITS.every(verifierPos => {
+    const mod =
+      generateChecksum(
+        cpf
+          .slice(0, verifierPos)
+          .split('')
+          .reduce((a, b) => a + b, ''),
+        verifierPos + 1
+      ) % 11;
+
+    return cpf[verifierPos] === String(mod < 2 ? 0 : 11 - mod);
+  });
+}
+
+export function isValidCPF(cpf: string) {
+  const digits = onlyNumbers(cpf);
+  return isValidCpfFormat(cpf) && !isReservedNumber(digits) && isValidChecksum(digits);
 }

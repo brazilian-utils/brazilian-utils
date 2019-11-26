f745475e02ecd8d43da1e1d10d05e54b4c7ca6a6
Hyan Mandian <hyan_mandian@sicredi.com.br>
2019-11-26 10:41:07 -0300

    migrate packages

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,16 +3,22 @@ import * as API from '.';
 describe('Public API', () => {
   const methods = [
     'formatCPF',
+    'isValidPIS',
     'isLastChar',
     'isValidCPF',
     'formatCNPJ',
     'isValidCNPJ',
     'onlyNumbers',
     'generateCPF',
+    'isValidEmail',
     'isValidPhone',
     'generateCNPJ',
+    'formatBankSlip',
+    'isValidBankSlip',
     'generateChecksum',
+    'isValidPostalCode',
     'generateRandomNumber',
+    'formatJudicialProcess',
   ];
 
   Object.keys(API).forEach(method => {

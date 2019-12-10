1b9ab8160cb9c4f19c51e63a38a76814b05f91c1
Matheus Almeida <mat.almeida@live.com>
2019-12-10 14:36:21 -0300

    finishes email validation package

--- src/utilities/email/index.ts
+++ src/utilities/email/index.ts
@@ -1,5 +1,30 @@
 export function isValid(email: string): boolean {
   if (!email || typeof email !== 'string') return false;
 
-  return /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/.test(email);
+  const maxRecipientLength = 64;
+  const maxDomainLength = 253;
+  const maxEmailLength = maxRecipientLength + 1 + maxDomainLength;
+
+  if (email.length > maxEmailLength) return false;
+
+  const validEmailRegex = /^([!#$%&'*+\-/=?^_`{|}~]{0,1}([a-zA-Z0-9][!#$%&'*+\-/=?^_`{|}~.]{0,1})+)@(([a-zA-Z0-9][-.]{0,1})+)([.]{1}[a-zA-Z0-9]+)$/;
+
+  const matchedEmail = validEmailRegex.exec(email);
+
+  if (!matchedEmail) return false;
+
+  const [
+    ,
+    // ignore
+    recipient, // ignore
+    ,
+    domain, // ignore
+    ,
+    topLevelDomain,
+  ] = matchedEmail;
+
+  if (recipient.length > maxRecipientLength) return false;
+  if (domain.length + topLevelDomain.length > maxDomainLength) return false;
+
+  return true;
 }

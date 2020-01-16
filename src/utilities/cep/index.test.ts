d8b0deb4aa9152db2bc32db200c11347da0c461d
Rafael Franco <rafaelfr@outlook.com>
2020-01-16 17:51:38 -0300

    feat: add cep formatter
    Add a method to format cep from a string.

--- src/utilities/cep/index.test.ts
+++ src/utilities/cep/index.test.ts
@@ -1,4 +1,26 @@
-import { isValid, LENGTH } from '.';
+import { format, isValid, LENGTH } from '.';
+
+describe('format', () => {
+  test('should format CEP with mask', () => {
+    expect(format('')).toBe('');
+    expect(format('0')).toBe('0');
+    expect(format('01')).toBe('01');
+    expect(format('010')).toBe('010');
+    expect(format('0100')).toBe('0100');
+    expect(format('01001')).toBe('01001');
+    expect(format('010010')).toBe('01001-0');
+    expect(format('0100100')).toBe('01001-00');
+    expect(format('01001000')).toBe('01001-000');
+  });
+
+  test(`should NOT add digits after the CEP length (${LENGTH})`, () => {
+    expect(format('01001000000000')).toBe('01001-000');
+  });
+
+  test('should remove all non numeric characters', () => {
+    expect(format('a0.10cr01?00#ab0')).toBe('01001-000');
+  });
+});
 
 describe('isValid', () => {
   describe('should return false', () => {
